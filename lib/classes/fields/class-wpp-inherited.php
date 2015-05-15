ba3ef0646b1771805e2f13729bad944953c8fe72
Maxim Peshkov <maxim.peshkov>
2015-05-15 13:01:17 +0000

    wip

--- lib/classes/fields/class-wpp-inherited.php
+++ lib/classes/fields/class-wpp-inherited.php
@@ -4,7 +4,28 @@ defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'RWMB_Wpp_Inherited_Field' ) )  {
 
-  class RWMB_Wpp_Inherited_Field extends RWMB_Wpp_Readonly_Field {
+  class RWMB_Wpp_Inherited_Field extends RWMB_Text_Field {
+
+    /**
+     * Get field HTML
+     *
+     * @param mixed $meta
+     * @param array $field
+     *
+     * @return string
+     */
+    static function html( $meta, $field ) {
+      return sprintf(
+        '<input type="text" readonly="readonly" name="%s" class="rwmb-text" id="%s" value="%s" placeholder="%s" size="%s" %s>%s',
+        $field['field_name'],
+        $field['id'],
+        $meta,
+        $field['placeholder'],
+        $field['size'],
+        $field['datalist'] ? "list='{$field['datalist']['id']}'" : '',
+        self::datalist_html( $field )
+      );
+    }
 
     /**
      * Get meta value
