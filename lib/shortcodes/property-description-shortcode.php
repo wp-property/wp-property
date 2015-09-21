d417d0a9b437186dcb1202bbf32db993d259bb90
Anton Korotkov <antonkorotkoff@gmail.com>
2015-09-21 18:53:41 +0300

    [ci skip] description shortcode added (wip)

---
+++ lib/shortcodes/property-description-shortcode.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Shortcode: [property_description]
+ *
+ * @since 2.0.5
+ */
+namespace UsabilityDynamics\WPP {
+
+  if( !class_exists( 'UsabilityDynamics\WPP\Property_Description_Shortcode' ) ) {
+
+    class Property_Description_Shortcode extends \UsabilityDynamics\Shortcode\Shortcode {
+
+      /**
+       * init
+       */
+      public function __construct() {
+
+        $options = array(
+            'id' => 'property_description',
+            'params' => array(),
+            'description' => __( 'Renders Property Description' ),
+            'group' => 'WP-Property'
+        );
+
+        parent::__construct( $options );
+      }
+
+      /**
+       * @param string $atts
+       * @return string|void
+       */
+      public function call( $atts = "" ) {
+
+        global $post;
+
+        ob_start();
+        echo $post->post_content;
+        return ob_get_clean();
+
+      }
+
+    }
+
+    /**
+     * Register
+     */
+    new Property_Description_Shortcode();
+
+  }
+
+}
