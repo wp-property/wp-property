80b45c61609590233832c49718d5fc581ca99098
alimuzzaman <alimuzzamanalim@gmail.com>
2015-11-20 22:20:59 +0600

    Property term widget Issue #182
    Property term widget Issue #182

--- static/views/property-terms.php
+++ static/views/property-terms.php
@@ -5,6 +5,9 @@
  * To modify it, copy it to your theme's root.
  */
 
+$list = get_the_term_list( $property_id, $taxonomy, '<li>', '</li><li>', '</li>' );
+if(trim($list) != ""):
 echo '<ul class="' . $taxonomy .'_list">';
-echo get_the_term_list( $property_id, $taxonomy, '<li>', '</li><li>', '</li>' );
+	echo $list;
 echo '</ul>';
+endif;
