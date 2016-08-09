cf746e092149004cf1b900ad56deaa903d201aed
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-08-09 17:53:07 -0400

    wp-property - removed sensor=true from google maps script path if when configuration.google_maps_api is set.

--- lib/class_core.php
+++ lib/class_core.php
@@ -188,7 +188,13 @@ class WPP_Core {
     wp_register_script( 'wp-property-backend-editor', WPP_URL . 'scripts/wpp.admin.editor.js', array( 'jquery', 'wp-property-global', 'wpp-localization' ), WPP_Version );
     wp_register_script( 'wp-property-global', WPP_URL . 'scripts/wpp.global.js', array( 'jquery', 'wpp-localization', 'jquery-ui-tabs', 'jquery-ui-sortable' ), WPP_Version );
     wp_register_script( 'jquery-cookie', WPP_URL . 'scripts/jquery.smookie.js', array( 'jquery', 'wpp-localization' ), '1.7.3' );
-    wp_register_script( 'google-maps', 'https://maps.google.com/maps/api/js?sensor=true&key='.ud_get_wp_property( 'configuration.google_maps_api' ) );
+
+    // Use Google Maps API Key, if provided.
+    if( ud_get_wp_property( 'configuration.google_maps_api' ) ) {
+      wp_register_script( 'google-maps', 'https://maps.google.com/maps/api/js?key='.ud_get_wp_property( 'configuration.google_maps_api' ) );
+    } else {
+      wp_register_script( 'google-maps', 'https://maps.google.com/maps/api/js?sensor=true' );
+    }
 
     wp_register_script( 'wpp-md5', WPP_URL . 'scripts/md5.js', array( 'wpp-localization' ), WPP_Version );
     wp_register_script( 'wpp-jquery-gmaps', WPP_URL . 'scripts/jquery.ui.map.min.js', array( 'google-maps', 'jquery-ui-core', 'jquery-ui-widget', 'wpp-localization' ) );
