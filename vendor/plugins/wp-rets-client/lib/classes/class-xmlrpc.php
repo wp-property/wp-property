a1aac7ba440819b99bcf055ab17bb46f5d2aaa60
Andy Potanin <andy@udx.io>
2016-11-01 14:57:14 +0200

    bugfix with lat/lon processing when already provided by api

--- vendor/plugins/wp-rets-client/lib/classes/class-xmlrpc.php
+++ vendor/plugins/wp-rets-client/lib/classes/class-xmlrpc.php
@@ -243,6 +243,13 @@ namespace UsabilityDynamics\WPRETSC {
 
         $post_data['tax_input'] = array();
 
+        // Ensure we have lat/log meta fields. @note May be a better place to set this up?
+        if( !$post_data[ 'meta_input' ][ 'latitude' ] && isset( $post_data['_system']['location']['lat'] ) ) {
+          $post_data[ 'meta_input' ][ 'latitude' ] = $post_data['_system']['location']['lat'];
+          $post_data[ 'meta_input' ][ 'longitude' ] = $post_data['_system']['location']['lon'];
+          ud_get_wp_rets_client()->write_log( 'Inserted lat/lon from _system ' . $post_data['_system']['location']['lat'] );
+        }
+
         $_post_id = wp_insert_post( $post_data, true );
 
         if( is_wp_error( $_post_id ) ) {
