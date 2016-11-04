0985ebc7bc0f0430be4dcd5abc1b58858d6a7a28
Anton Korotkov <antonkorotkoff@gmail.com>
2016-11-04 18:52:49 +0200

    wip

--- lib/classes/class-layouts.php
+++ lib/classes/class-layouts.php
@@ -11,17 +11,14 @@ namespace UsabilityDynamics\WPP {
      * Class Layouts
      * @package UsabilityDynamics\WPP
      */
-    final class Layouts extends Scaffold
-    {
+    final class Layouts extends Scaffold {
 
       /**
        * Layouts constructor.
        */
-      public function __construct()
-      {
+      public function __construct() {
         parent::__construct();
 
-        add_action('init', array($this, 'register_layout_post_type'));
         add_filter('template_include', array($this, 'page_template'), 99);
         add_action('wp_footer', array($this, 'panels_print_inline_css'));
       }
@@ -559,42 +556,6 @@ namespace UsabilityDynamics\WPP {
         }
 
         return $style_wrapper;
-      }
-
-      /**
-       * Register post type
-       */
-      public function register_layout_post_type()
-      {
-
-        $labels = array(
-            'name' => _x('Layouts', 'post type general name', ud_get_wp_property()->domain),
-            'singular_name' => _x('Layout', 'post type singular name', ud_get_wp_property()->domain),
-            'menu_name' => _x('Layouts', 'admin menu', ud_get_wp_property()->domain),
-            'name_admin_bar' => _x('Layout', 'add new on admin bar', ud_get_wp_property()->domain),
-            'add_new' => _x('Add New', 'Layout', ud_get_wp_property()->domain),
-            'add_new_item' => __('Add New Layout', ud_get_wp_property()->domain),
-            'new_item' => __('New Layout', ud_get_wp_property()->domain),
-            'edit_item' => __('Edit Layout', ud_get_wp_property()->domain),
-            'view_item' => __('View Layout', ud_get_wp_property()->domain),
-            'all_items' => __('Layouts', ud_get_wp_property()->domain),
-            'search_items' => __('Search Layouts', ud_get_wp_property()->domain),
-            'parent_item_colon' => __('Parent Layouts:', ud_get_wp_property()->domain),
-            'not_found' => __('No Layouts found.', ud_get_wp_property()->domain),
-            'not_found_in_trash' => __('No Layouts found in Trash.', ud_get_wp_property()->domain)
-        );
-
-        register_post_type('wpp_layout', array(
-            'public' => false,
-            'show_ui' => true,
-            'description' => __('Layouts for property pages.'),
-            'can_export' > true,
-            'rewrite' => false,
-            'labels' => $labels,
-            'show_in_menu' => 'edit.php?post_type=property',
-            'supports' => array('title', 'editor', 'revisions')
-        ));
-
       }
     }
   }
