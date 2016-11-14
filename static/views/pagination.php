7a5c60b495e77db33e6c8c41b639e6c0fb7d4181
SoSeductive <den.aka.dizel@gmail.com>
2016-11-14 15:30:33 +0200

    Load more button pagination

--- static/views/pagination.php
+++ static/views/pagination.php
@@ -6,49 +6,56 @@
  * To modify template, copy the file to root of your theme.
  */
 ?>
-<div class="properties_pagination <?php echo $settings[ 'class' ]; ?> wpp_slider_pagination type-<?php echo $settings[ 'type' ]; ?>">
+<div class="properties_pagination <?php echo $settings['class']; ?> wpp_slider_pagination type-<?php echo $settings['type']; ?>">
 
+  <?php if ($settings['type'] !== 'loadmore' || ($settings['type'] == 'loadmore' && $settings['class'] !== 'wpp_bottom_pagination')) { ?>
   <div class="wpp_pagination_slider_status">
     <span class="wpp_property_results_options">
-      <?php if ( $hide_count != 'true' ) {
+      <?php if ($hide_count != 'true') {
         $wpp_property_results = '<span class="wpp_property_results">';
-        $wpp_property_results .= ( $properties[ 'total' ] > 0 ? \WPP_F::format_numeric( $properties[ 'total' ] ) : __( 'None', ud_get_wp_property()->domain ) );
-        $wpp_property_results .= __( ' found.', ud_get_wp_property()->domain );
-        echo apply_filters( 'wpp::wpp_draw_pagination::wpp_property_results', $wpp_property_results, array( 'properties' => $properties, 'settings' => $settings ) );
+        $wpp_property_results .= ($properties['total'] > 0 ? \WPP_F::format_numeric($properties['total']) : __('None', ud_get_wp_property()->domain));
+        $wpp_property_results .= __(' found.', ud_get_wp_property()->domain);
+        echo apply_filters('wpp::wpp_draw_pagination::wpp_property_results', $wpp_property_results, array('properties' => $properties, 'settings' => $settings));
       } ?>
-      <?php if ( !empty( $use_pagination ) ) { ?>
-        <?php _e( 'Viewing page', ud_get_wp_property()->domain ); ?>
-        <span class="wpp_current_page_count">1</span> <?php _e( 'of', ud_get_wp_property()->domain ); ?>
+      <?php if (!empty($use_pagination)) { ?>
+        <?php _e('Viewing page', ud_get_wp_property()->domain); ?>
+        <span class="wpp_current_page_count">1</span> <?php _e('of', ud_get_wp_property()->domain); ?>
         <span class="wpp_total_page_count"><?php echo $pages; ?></span>.
       <?php } ?>
     </span>
-    <?php if ( $sortable_attrs ) { ?>
-      <span class="wpp_sorter_options"><span class="wpp_sort_by_text"><?php echo $settings[ 'sort_by_text' ]; ?></span>
-        <?php if ( $settings[ 'sorter_type' ] == 'buttons' ) { ?>
-          <?php foreach ( $sortable_attrs as $slug => $label ) { ?>
-            <span class="wpp_sortable_link <?php echo( $sort_by == $slug ? 'wpp_sorted_element' : '' ); ?> label label-info" sort_order="<?php echo $sort_order ?>" sort_slug="<?php echo $slug; ?>"><?php echo $label; ?></span>
+    <?php if ($sortable_attrs) { ?>
+      <span class="wpp_sorter_options"><span class="wpp_sort_by_text"><?php echo $settings['sort_by_text']; ?></span>
+        <?php if ($settings['sorter_type'] == 'buttons') { ?>
+          <?php foreach ($sortable_attrs as $slug => $label) { ?>
+            <span
+              class="wpp_sortable_link <?php echo($sort_by == $slug ? 'wpp_sorted_element' : ''); ?> label label-info"
+              sort_order="<?php echo $sort_order ?>" sort_slug="<?php echo $slug; ?>"><?php echo $label; ?></span>
           <?php } ?>
-        <?php } elseif ( $settings[ 'sorter_type' ] == 'dropdown' ) { ?>
+        <?php } elseif ($settings['sorter_type'] == 'dropdown') { ?>
           <select class="wpp_sortable_dropdown sort_by label-info" name="sort_by">
-            <?php foreach ( $sortable_attrs as $slug => $label ) { ?>
-              <option <?php echo( $sort_by == $slug ? 'class="wpp_sorted_element" selected="true"' : '' ); ?> sort_slug="<?php echo $slug; ?>" value="<?php echo $slug; ?>"><?php echo $label; ?></option>
+            <?php foreach ($sortable_attrs as $slug => $label) { ?>
+              <option <?php echo($sort_by == $slug ? 'class="wpp_sorted_element" selected="true"' : ''); ?>
+                sort_slug="<?php echo $slug; ?>" value="<?php echo $slug; ?>"><?php echo $label; ?></option>
             <?php } ?>
           </select>
         <?php } else { ?>
-          <?php do_action( 'wpp_custom_sorter', array( 'settings' => $settings, 'wpp_query' => $wpp_query, 'sorter_type' => $settings[ 'sorter_type' ] ) ); ?>
+          <?php do_action('wpp_custom_sorter', array('settings' => $settings, 'wpp_query' => $wpp_query, 'sorter_type' => $settings['sorter_type'])); ?>
         <?php } ?>
         </span>
     <?php } ?>
     <div class="clear"></div>
   </div>
+  <?php } ?>
 
   <?php /* Render Pagination Template based on pagination type */
-  if ( !empty( $use_pagination ) ) {
-    $template = self::get_pagination_template_based_on_type( $settings[ 'type' ] );
-    if( file_exists( $template ) ) {
-      include( $template );
-    } else {
-      _e( 'No Pagination Template Found', ud_get_wp_property('domain') );
+  if (!empty($use_pagination)) {
+    if ($settings['type'] !== 'loadmore' || ($settings['type'] == 'loadmore' && $settings['class'] == 'wpp_bottom_pagination')) {
+      $template = self::get_pagination_template_based_on_type($settings['type']);
+      if (file_exists($template)) {
+        include($template);
+      } else {
+        _e('No Pagination Template Found', ud_get_wp_property('domain'));
+      }
     }
   }
   ?>
