e2d2f183b59f1240f4d05ed883dee4064ee56928
Andy Potanin <andy@udx.io>
2017-02-24 16:00:53 -0500

    wp-property - making read-only taxonomy term-editing hiding dynamic

--- lib/classes/class-admin.php
+++ lib/classes/class-admin.php
@@ -174,13 +174,18 @@ namespace UsabilityDynamics\WPP {
           return;
         }
 
-        // Hide term editing UI.
-        if( $current_screen->base === 'edit-tags' && $current_screen->taxonomy === 'wpp_categorical' ) {
-          return 'wpp-disable-term-editing';
+        $_readonly_taxonomies = array();
+
+        foreach( $wp_properties['taxonomies'] as $_tax => $_tax_detail ) {
+
+          if( $_tax_detail['readonly'] === 'true' || $_tax_detail['readonly'] == 1 || $_tax_detail['readonly'] === '1' ) {
+            $_readonly_taxonomies[] = $_tax;
+          }
         }
 
-        if( $current_screen->base === 'edit-tags' && $current_screen->taxonomy === 'wpp_listing_location' ) {
-          return 'wpp-disable-term-editing';
+        // Hide term editing UI.
+        if( $current_screen->base === 'edit-tags' && in_array($current_screen->taxonomy, $_readonly_taxonomies ) ) {
+          return 'wpp-disable-term-editing wpp-readonly-taxonomy';
         }
 
       }
