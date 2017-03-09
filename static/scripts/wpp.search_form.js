dc46d3d8e8ca59d8a9c9bd71c19efbd8a7642b9f
SoSeductive <den.aka.dizel@gmail.com>
2017-03-09 12:59:46 +0200

    Property search widget v2

--- static/scripts/wpp.search_form.js
+++ static/scripts/wpp.search_form.js
@@ -3,9 +3,9 @@
  *
  */
 jQuery(document).ready(function () {
-  jQuery('.wpp_search_elements > li.wpp_search_group:first-child').addClass('active');
+  jQuery('.wpp_search_properties_widget_v2 .wpp_search_elements > li.wpp_search_group:first-child').addClass('active');
 
-  jQuery(document).on('click', '.wpp_search_elements > li.wpp_search_group > span.wpp_search_group_title', function() {
+  jQuery(document).on('click', '.wpp_search_properties_widget_v2 .wpp_search_elements > li.wpp_search_group > span.wpp_search_group_title', function() {
     jQuery(this).parent('li.wpp_search_group').toggleClass('active');
   })
 });
