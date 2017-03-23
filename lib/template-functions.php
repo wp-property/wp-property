a68657a237f0a8d41e589a7a29e9de31a78ecafb
SoSeductive <den.aka.dizel@gmail.com>
2017-03-23 16:20:18 +0200

    Some fixes in templates

--- lib/template-functions.php
+++ lib/template-functions.php
@@ -1125,9 +1125,9 @@ if (!function_exists('draw_property_search_form')):
       }
     }
     if (WPP_LEGACY_WIDGETS) { // If used old widget
-      include_once WPP_Path . 'static/views/property-search-form.php';
+      include ud_get_wp_property()->path('static/views/property-search-form.php', 'dir');
     } else { //If used new widget
-      include_once WPP_Path . 'static/views/property-search-form-v2.php';
+      include ud_get_wp_property()->path('static/views/property-search-form-v2.php', 'dir');
     }
   }
 endif;
